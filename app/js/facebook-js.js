f2d676f2f7e5a59fba3e32d884137ded848df1c6
dani5447 <drn5447@rit.edu>
2015-09-29 20:11:49 -0400

    Starting basic passport-facebook integration

--- app/js/facebook-js.js
+++ app/js/facebook-js.js
@@ -2,7 +2,6 @@
 
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
     console.log(response);
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
