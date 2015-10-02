5febe7f7ece19dd55d32a9b37afdff87a5fa9dc0
Jesse Jurman <j.r.jurman@gmail.com>
2015-10-02 02:46:59 -0400

    add new home page and events

---
+++ app/js/components/event.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Event extends Component {
+  render() {
+    var dateString = this.props.day + " " + this.props.startTime + (this.props.endTime ? "-" + this.props.endTime : "")
+
+    return (
+      <div className="panel panel-primary">
+        <div className="panel-heading">
+          <h3 className="panel-title">{this.props.title}</h3>
+        </div>
+        <div className="panel-body">
+          <p className="text-primary">{dateString}</p>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
--- app/js/index.js
+++ app/js/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import Router from 'react-router';
 
+import Navbar from './components/Navbar';
+import Event from './components/Event';
+
 const {Route, RouteHandler} = Router;
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>asdf</h1>
+        <Navbar page="Home"/>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-4">
+              <h1>Stocks</h1>
+            </div>
+            <div className="col-xs-4">
+              <h1>Feed</h1>
 
+            </div>
+            <div className="col-xs-4">
+              <h1>Calendar</h1>
+              <Event title="R1 Release" day="Monday" startTime="12:00pm">
+                <p> We have to release our awesome web-app! </p>
+              </Event>
+              <Event title="Ice Cream Event" day="Tuesday" startTime="12:00pm" endTime="1:00pm" />
+              <Event title="Watch Movies" day="Wednesday" startTime="3:00am" endTime="9:00pm">
+                <p> Watching Movies All Day Long! </p>
+              </Event>
+            </div>
+          </div>
+        </div>
         <RouteHandler/>
       </div>
     );
