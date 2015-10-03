b993d1e54515270c0b1bb50a5315b3439d0c94b2
cousincuey <caffinefree19@yahoo.com>
2015-10-03 18:20:48 -0400

    got rid of a warning

--- test/zombieTest.js
+++ test/zombieTest.js
@@ -8,7 +8,7 @@ describe('User visits landing page', function(){
 
 	before(function(done){
 		server.listen(3001, () => {
-			browser.visit('http://localhost:3001/', done);
+			browser.visit('http://localhost:3001/#/', done);
 		});
 	});
 
