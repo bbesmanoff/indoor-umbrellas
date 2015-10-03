90262a80876042442349c79bb86c42c4a5cdd3c2
cousincuey <caffinefree19@yahoo.com>
2015-10-03 17:08:28 -0400

    added /# to the localhost url

--- test/zombieTest.js
+++ test/zombieTest.js
@@ -8,7 +8,7 @@ describe('User visits landing page', function(){
 	before(function(done){
 		const port = process.env.PORT || 3001;
 		server.listen(port,'');
-		browser.visit('http://localhost:3001/',done);
+		browser.visit('http://localhost:3001/#/',done);
 	});
 
 	it('Successfull landing', function() {
