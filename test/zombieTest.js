0cd9631817c840e847aab31f974fd8958def6468
cousincuey <caffinefree19@yahoo.com>
2015-10-03 16:53:52 -0400

    start server in  zombie

--- test/zombieTest.js
+++ test/zombieTest.js
@@ -2,10 +2,14 @@ const Browser = require('zombie');
 
 Browser.localhost('indoor-umbrellas',3000);
 
+import server from '../server';
+
 describe('User visits landing page', function(){
 	const browser = new Browser();
 
 	before(function(done){
+		const port = process.env.PORT || 3001;
+		server.listen(port,'');
 		browser.visit('/',done);
 	});
 
