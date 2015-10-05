1554d639fdb6c8c173581a9af7d21ecbe3321383
Brian Besmanoff <bbesmanoff@gmail.com>
2015-10-04 23:01:20 -0400

    Zero-out template

--- app/js/stocks.js
+++ app/js/stocks.js
@@ -2,19 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Navbar from './components/navbar';
-import EventList from './components/event-list';
 
 class App extends React.Component {
 
   render() {
-    var today = new Date();
-    var tomorrow = (new Date()).setDate(today.getDate() + 1);
-
     return (
       <div>
         <Navbar page="Stocks"/>
-        <div className="container">
-        </div>
       </div>
     );
   }
