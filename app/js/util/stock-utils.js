e177d5ca037333386ef0df6d8fad740836b0eae8
Brian Besmanoff <bbesmanoff@gmail.com>
2015-10-05 16:16:59 -0400

    Extract getting stock data to a utility method

---
+++ app/js/util/stock-utils.js
@@ -0,0 +1,20 @@
+/**
+* Makes a request to the API to retrieve a single stock object.
+*
+* @param {string} symbol
+* @return {Promise}
+*/
+export function getStockData(symbol) {
+  return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', `/api/stocks/${symbol}`);
+      xhr.onload = () => {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject({status: xhr.status, error: response.errorText});
+        }
+      };
+      xhr.send();
+  });
+};
