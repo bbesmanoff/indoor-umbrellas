7d5496e1b1e153663194d0f9c0e43f2973fb53c0
Brian Besmanoff <bbesmanoff@gmail.com>
2015-10-05 01:01:05 -0400

    Implement StockDetails

--- app/js/components/StockDetails.js
+++ app/js/components/StockDetails.js
@@ -1,10 +1,51 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
 
 export default class StockDetails extends Component {
+  renderNoStock() {
+    return (
+      <strong>
+        Please search using the search bar (above|to the left).
+      </strong>
+    );
+  }
+
+  renderQuandlError() {
+    return (
+      <div class="alert alert-danger">
+        <strong>
+          Something went wrong when reading stock data.  Please check your input and try again.
+        </strong>
+      </div>
+    );
+  }
+
   render() {
+    if (this.props.stock == null) {
+      return this.renderNoStock();
+    };
+
+    if (this.props.stock.quandl_error) {
+      return this.renderQuandlError();
+    }
+
+    const stock = this.props.stock.dataset;
+    const pairs = _.zip(stock.column_names, stock.data[0]).map((e) => {
+    return (
+      <tr key={e.join('')}>
+        <td>{e[0]}</td>
+        <td>{e[1]}</td>
+      </tr>
+    )});
+
     return (
       <div>
-        StockDetails
+        <h1>{stock.name}</h1>
+        <table>
+          <tbody>
+            {pairs}
+          </tbody>
+        </table>
       </div>
     );
   }
