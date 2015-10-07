fc50688a4e669ccf76ae90a51335445f7a147039
Brian Besmanoff <bbesmanoff@gmail.com>
2015-10-07 12:15:28 -0400

    Allow enter to submit in stock search
    Closes #70

--- app/js/components/stock-search-bar.js
+++ app/js/components/stock-search-bar.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 
 export default class StockSearchBar extends Component {
+  onTextChange(e) {
+    if (e.nativeEvent.keyCode === 13) {
+      this.props.onSearch();
+    }
+  }
+
   render() {
     return (
       <div>
-        <input type='text' id='stock-search-input' />
+        <input type='text' id='stock-search-input'
+          onKeyPress={this.onTextChange.bind(this)}/>
         <button onClick={this.props.onSearch}>Search</button>
       </div>
     );
