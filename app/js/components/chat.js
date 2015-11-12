73e76be2413e44bc5f94df099e420035f471b1c6
dani5447 <drn5447@rit.edu>
2015-11-11 22:08:57 -0500

    Fixes #104, Chat cookie get method change

--- app/js/components/chat.js
+++ app/js/components/chat.js
@@ -57,14 +57,8 @@ export default class Chat extends React.Component {
   }
     
   getCookieValue(cname){
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-      var c = ca[i];
-      c.trim();
-      if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-    }
-    return "";
+    var match = document.cookie.match(new RegExp(cname + '=([^;]+)'));
+    if (match) return match[1];
   }
     
   postIt(e) {
