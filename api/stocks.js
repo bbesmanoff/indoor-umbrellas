0a99b1036a79931aef7ec637d61281051f47906e
Brian Besmanoff <bbesmanoff@gmail.com>
2015-11-21 12:32:48 -0500

    Add a `POST /transactions` endpoint

--- api/stocks.js
+++ api/stocks.js
@@ -1,7 +1,23 @@
 import express from 'express';
 import Quandl from 'quandl';
+import * as db from './models';
+import {invariant} from './utils/stocks';
+import bodyParser from 'body-parser';
 
 const stocksEndpoint = express.Router();
+stocksEndpoint.use(bodyParser.json());
+
+stocksEndpoint.post('/transactions', (req, res) => {
+  db.StockLedger.findAll({user_id: req.user.id}).then((transactions) => {
+    const transaction = {...req.body.transaction, user_id: req.user.id};
+
+    if (invariant([...transactions, transaction])) {
+      db.StockLedger.create(transaction).then(() => res.sendStatus(200));
+    } else {
+      res.sendStatus(422); // Unprocessible Entry
+    }
+  });
+});
 
 stocksEndpoint.get('/top-stocks', (req, res) => {
   res.send(JSON.stringify(['XRX', 'XOM', 'GOOG', 'AMZN', 'AAPL']));
